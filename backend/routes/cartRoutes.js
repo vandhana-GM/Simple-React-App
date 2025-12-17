5225a02fd58bec4a7af4d6476612a49933790699
vandhana-GM <vandhana.gm2024aiml@sece.ac.in>
2025-12-17 12:39:43 +0530

    Update cartRoutes.js

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/authMiddleware");
+const auth = require("../middleware/authmiddleware");
 const Cart = require("../models/cart");
 
 // SAVE CART
