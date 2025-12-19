d4517ace00c8f665ca54cc5c425ffa4006839e9e
vandhana-GM <vandhana.gm2024aiml@sece.ac.in>
2025-12-19 09:22:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors(
+  {
+        origin:"http://13.62.127.110:7320",
+        credentials:true
+    }
+));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
